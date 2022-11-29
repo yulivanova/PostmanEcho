84c3aa5ae32a06e3520d5c559e18e60ed828ae3b
Юлия <jullka5@mail.ru>
2022-11-29 23:14:25 +0300

    first commit

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -13,7 +13,7 @@ public class PostmanEchoTest {
         given()
                 .baseUri("https://postman-echo.com")
                 .contentType("text/plain; charset=UTF-8")
-                .body("PostMan")
+                .body("some data")
                 .when()
                 .post("/post")
                 .then()
